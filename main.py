2c7a9f6f4d28f65d138c6fced1a4c3182a4aa1ca
kauafabricio <kauafabriiciio@gmail.com>
2026-05-06 00:42:14 -0300

    ajustes main.py

--- main.py
+++ main.py
@@ -2,25 +2,33 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.controllers.auth_controller import router as auth_router
 
-app = FastAPI()
+app = FastAPI(
+    title="Ruralize API",
+    description="API para gerenciamento de ações sustentáveis na UFRPE",
+    version="1.0.0"
+)
 
-# 🔥 CONFIGURAÇÃO CORS
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://ruralize-ufrpe.vercel.app",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # pode usar ["*"] pra testar
+    allow_origins=origins, 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# rota de autenticação
 app.include_router(auth_router, prefix="/auth", tags=["Auth"])
 
 @app.get("/")
 def home():
-    return {"message": "API rodando"}
+    return {
+        "name": "Ruralize API",
+        "status": "online",
+        "docs": "/docs",
+        "version": "1.0.0"
+    }
