9e57ad518c78826ed4e65ee549575111687edd2f
kauafabricio <kauafabriiciio@gmail.com>
2026-05-05 23:51:29 -0300

    cors public to test

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.controllers.auth_controller import router as auth_router
 
 app = FastAPI()
+
+# 🔥 CONFIGURAÇÃO CORS
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # pode usar ["*"] pra testar
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # rota de autenticação
 app.include_router(auth_router, prefix="/auth", tags=["Auth"])
